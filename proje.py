995d37ddc305e7c2511adf2bb226542855bad1aa
gorkembatinoner <hotz.123@hotmail.com>
2025-04-30 15:45:54 +0300

    dlib+cv2_import

--- proje.py
+++ proje.py
@@ -1,19 +1,27 @@
-from scipy.spatial import distance
 import pygame
 import os
+import cv2
+import dlib
+from scipy.spatial import distance
 
 def calculate_EAR(eye):
-    """Göz açıklık oranını (EAR) hesaplar"""
     A = distance.euclidean(eye[1], eye[5])
     B = distance.euclidean(eye[2], eye[4])
     C = distance.euclidean(eye[0], eye[3])
-    ear_aspect_ratio = (A + B) / (2.0 * C)
-    return ear_aspect_ratio
+    return (A + B) / (2.0 * C)
 
 def main():
     pygame.mixer.init()
-    alarm_sound=pygame.mixer.Sound("alarm.wav")
-    
-    if not os.path.exists("Shape_predictor_68_face_landmarks.dat"):
+    alarm_sound = pygame.mixer.Sound("alarm.wav")
+
+    if not os.path.exists("shape_predictor_68_face_landmarks.dat"):
         print("Hata: shape_predictor_68_face_landmarks.dat dosyası bulunamadı!")
         return
+
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Hata: Kamera açılamadı!")
+        return
+
+    hog_face_detector = dlib.get_frontal_face_detector()
+    dlib_facelandmark = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
