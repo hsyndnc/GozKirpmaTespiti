4942adb1a79284cfb053556ac7b8a9e455604b2e
Hüseyin Dinç <huseyinndnc@gmail.com>
2025-04-30 18:49:09 +0300

    Göz Tespiti yapıldı

--- proje.py
+++ proje.py
@@ -25,3 +25,46 @@ def main():
 
     hog_face_detector = dlib.get_frontal_face_detector()
     dlib_facelandmark = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Hata: Kameradan görüntü alınamadı!")
+            break
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = hog_face_detector(gray)
+
+        for face in faces:
+            landmarks = dlib_facelandmark(gray, face)
+            leftEye = []
+            rightEye = []
+
+            for n in range(36, 42):
+                x = landmarks.part(n).x
+                y = landmarks.part(n).y
+                leftEye.append((x, y))
+                next_point = 36 if n == 41 else n + 1
+                x2 = landmarks.part(next_point).x
+                y2 = landmarks.part(next_point).y
+                cv2.line(frame, (x, y), (x2, y2), (0, 255, 0), 1)
+
+            for n in range(42, 48):
+                x = landmarks.part(n).x
+                y = landmarks.part(n).y
+                rightEye.append((x, y))
+                next_point = 42 if n == 47 else n + 1
+                x2 = landmarks.part(next_point).x
+                y2 = landmarks.part(next_point).y
+                cv2.line(frame, (x, y), (x2, y2), (0, 255, 0), 1)
+        
+        # test kodu
+        left_ear = calculate_EAR(leftEye)
+        right_ear = calculate_EAR(rightEye)
+        ear = (left_ear + right_ear) / 2.0
+        print(f"EAR: {ear:.2f}")
+
+
+        cv2.imshow("Görüntü", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
